747d60bab841ca69fe8d08a83af387998ca79099
vjdw <github_vjdw@vwingate.sent.com>
2016-05-28 13:05:58 +0100

    Tidied up, removed hardcoded hostnames.

--- hubbub.go
+++ hubbub.go
@@ -9,66 +9,22 @@ import (
 )
 
 func main() {
-	flagClient := flag.Bool("c", false, "Client mode.")
+	flagHostname := flag.String("h", "", "Hubbub server hostname")
 	flag.Parse()
-	fmt.Printf("Client mode: ", *flagClient)
 
-	if *flagClient {
-		runClient()
+	if len(*flagHostname) > 0 {
+		fmt.Printf("Server hostname: %s\n", *flagHostname)
+		runClient(*flagHostname)
 	} else {
-		recss := pulse.SampleSpec{pulse.SAMPLE_S16LE, 44100, 1}
-		rec, err := pulse.Capture2("pulse-simple record", "pulse-simple record", &recss, "alsa_output.pci-0000_00_07.0.analog-stereo.monitor")
-		defer rec.Free()
-		defer rec.Drain()
-		if err != nil {
-			fmt.Printf("Could not create record stream: %s\n", err)
-			return
-		}
-
-		for {
-			data := make([]byte, 1*recss.Rate)
-			rec.Read(data)
-
-			dataCopy := make([]byte, 1*recss.Rate)
-			copy(dataCopy, data)
-			go func() {
-				sendToClient(&dataCopy)
-			}()
-		}
-
-		// for {
-		// 	data := make([]byte, 1*recss.Rate)
-		// 	rec.Read(data)
-		// 	sendToClient(&data)
-		// }
+		fmt.Printf("No hostname specified, running in server mode.\n")
+		runServer()
 	}
 }
 
-func sendToClient(data *[]byte) {
-	//conn, err := net.Dial("udp", "llamatron:1236")
-	addr, err := net.ResolveUDPAddr("udp", "llamatron:1234")
-	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
-	}
-	conn, err := net.DialUDP("udp", nil, addr)
-
-	//fmt.Print(*data)
-	conn.Write(*data)
-	//fmt.Fprintf(conn, "Hi UDP client, How are you doing?")
-	conn.Close()
-}
-
-func sendToServer(conn *net.UDPConn, addr *net.UDPAddr) {
-	_, err := conn.WriteToUDP([]byte("From client: Hello I got your mesage "), addr)
-	if err != nil {
-		fmt.Printf("Couldn't send response %v", err)
-	}
-}
-
-func runClient() {
+func runClient(server string) {
+	// Connect to pulseaudio.
 	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, 44100, 1}
-	pb, err := pulse.Playback("pulse-simple test", "playback test", &ss)
+	pb, err := pulse.Playback("Hubbub Client", "Hubbub Client", &ss)
 	defer pb.Free()
 	defer pb.Drain()
 	if err != nil {
@@ -76,41 +32,89 @@ func runClient() {
 		return
 	}
 
-	p := make([]byte, 1*ss.Rate)
-
-	addr, err := net.ResolveUDPAddr("udp", ":1234")
+	// Open connection to Hubbub server.
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", server, 1234))
 	if err != nil {
 		fmt.Printf("Some error %v\n", err)
 		return
 	}
-
-	// addr := net.UDPAddr{
-	//     Port: 1236,
-	//     IP: net.ParseIP("127.0.0.1"),
-	// }
-
 	ser, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		fmt.Printf("Some error %v\n", err)
 		return
 	}
+	send(server, 1235, []byte("register"))
 
 	fmt.Printf("Started listening...")
+	p := make([]byte, 1*ss.Rate)
 	for {
 		_, remoteaddr, err := ser.ReadFromUDP(p)
 		if err != nil {
 			fmt.Printf("Some error  %v", err)
 			continue
 		}
-		//fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
 		fmt.Printf("Read a message from %v\n", remoteaddr)
 
 		pCopy := make([]byte, 1*ss.Rate)
 		copy(pCopy, p)
 		go func() {
-			//go sendToServer(ser, remoteaddr)
 			pb.Write(pCopy)
 			return
 		}()
 	}
+}
+
+func runServer() {
+	client := waitForClient()
+
+	// Connect to pulseaudio.
+	recss := pulse.SampleSpec{pulse.SAMPLE_S16LE, 44100, 1}
+	rec, err := pulse.Capture2("Hubbub Server", "Hubbub Server", &recss, "alsa_output.pci-0000_00_07.0.analog-stereo.monitor")
+	defer rec.Free()
+	defer rec.Drain()
+	if err != nil {
+		fmt.Printf("Could not create record stream: %s\n", err)
+		return
+	}
+
+	for {
+		data := make([]byte, 1*recss.Rate)
+		rec.Read(data)
+
+		dataCopy := make([]byte, 1*recss.Rate)
+		copy(dataCopy, data)
+		go send(client, 1234, dataCopy)
+	}
+}
+
+func waitForClient() string {
+	fmt.Printf("Waiting for client to register.\n")
+	addr, err := net.ResolveUDPAddr("udp", ":1235")
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return ""
+	}
+	ser, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return ""
+	}
+	p := make([]byte, 8)
+	_, remoteaddr, err := ser.ReadFromUDP(p)
+	fmt.Printf("%s request received from %v\n", p, remoteaddr)
+	client := remoteaddr.IP.String()
+
+	return client
+}
+
+func send(host string, port int, data []byte) {
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+
+	conn.Write(data)
+	conn.Close()
 }
